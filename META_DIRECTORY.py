dc21493dcda138887eca4bdd83fc30df0c6ff1a4
XiaoyuJIN97 <79083820+XiaoyuJIN97@users.noreply.github.com>
2023-07-08 16:07:33 +0800

    META_DIRECTORY.py

--- META_DIRECTORY.py
+++ META_DIRECTORY.py
@@ -81,7 +81,7 @@ st.markdown(""" """)
 st.markdown(""" """)
 st.markdown(""" """)
 st.markdown("""*Any questions? Contact us📧: xiaoyu.jin@connect.polyu.hk*""")
-st.markdown("""<font size="7">*Acknowledge for the support by the National Key Research and Development Program of China (2021YFE0107400)*</font>""")
+st.markdown("""*<p style="font-size: 42px;">*Acknowledge for the support by the National Key Research and Development Program of China (2021YFE0107400)*</p>""")
 #%%
 def update_the_spreadsheet(spreadsheetname,dataframe):
     #col = ['Dataset Full Name','URL']
