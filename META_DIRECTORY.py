deb6cc637615c77a4fac27924409d6675543f2de
XiaoyuJIN97 <79083820+XiaoyuJIN97@users.noreply.github.com>
2023-10-27 15:28:46 +0800

    META_DIRECTORY.py

--- META_DIRECTORY.py
+++ META_DIRECTORY.py
@@ -81,13 +81,17 @@ st.write(users_html, unsafe_allow_html=True)
 st.markdown(""" """)
 st.markdown(""" """)
 st.markdown(""" """)
-st.markdown("""To cite the Building Data Genome Directory:\n
- The Building Data Genome Directory – An open, comprehensive data sharing platform for building performance research.arXiv:2307.00793 [stat.AP](or arXiv:2307.00793v1 [stat.AP] for this version\n)
+st.markdown("""<p style="font-size: 16px;">To cite the Building Data Genome Directory:*</p>
+The Building Data Genome Directory – An open, comprehensive data sharing platform for building performance research.arXiv:2307.00793 [stat.AP](or arXiv:2307.00793v1 [stat.AP] for this version\n)
 https://doi.org/10.48550/arXiv.2307.00793\n
-Other relevant publication:\n
+
+<p style="font-size: 16px;">Other relevant publication:*</p>
 A review and reflection on open datasets of city-level building energy use and their applications. https://10.1016/j.enbuild.2023.112911""")
-st.markdown("""*Any questions or ideas? Join our GitHub community at [building-data-directory Discussions](https://github.com/buds-lab/building-data-directory/discussions)""")
+
+st.markdown(""" """)
+st.markdown("""*Any questions or ideas? Join our GitHub community at [building-data-directory Discussions](https://github.com/buds-lab/building-data-directory/discussions)*""")
 st.markdown("""*Further questions? Contact us📧: xiaoyu.jin@connect.polyu.hk*""")
+st.markdown(""" """)
 st.markdown("""*<p style="font-size: 12px;">Acknowledge for the support by the National Key Research and Development Program of China (2021YFE0107400), and the joint effort of the Annex 81 community.*</p>""",unsafe_allow_html=True)
 #%%
 def update_the_spreadsheet(spreadsheetname,dataframe):
