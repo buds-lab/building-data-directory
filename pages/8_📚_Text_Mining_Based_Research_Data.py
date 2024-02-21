cd86cdfd62611ef0d00ba0d3e7b063062a8135ed
XiaoyuJIN97 <79083820+XiaoyuJIN97@users.noreply.github.com>
2024-02-21 14:30:42 +0800

    Update 8_📚_Text_Mining_Based_Research_Data.py

--- pages/8_📚_Text_Mining_Based_Research_Data.py
+++ pages/8_📚_Text_Mining_Based_Research_Data.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Oct 26 10:26:12 2022
 
-@author: 靳笑宇
+@author: Xiaoyu Jin
 """
 
 import streamlit as st
@@ -14,7 +14,7 @@ import plotly.express as px
 from gspread_pandas import Spread,Client
 from google.oauth2 import service_account
 import ssl 
-
+from streamlit_gsheets import GSheetsConnection
 st.set_page_config(
     page_title="Text Mining Based Research Data"
 )
@@ -37,20 +37,23 @@ def filedownload(df):
 #Link to google sheet
 #%%
 #Link to google sheet
-ssl._create_default_https_context = ssl._create_unverified_context
+#ssl._create_default_https_context = ssl._create_unverified_context
 
 # Create a Google Authentication connection object
-scope = ["https://www.googleapis.com/auth/spreadsheets",
-         "https://www.googleapis.com/auth/drive"]
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=scope)
-client = Client(scope=scope,creds=credentials)
+#scope = ["https://www.googleapis.com/auth/spreadsheets",
+#         "https://www.googleapis.com/auth/drive"]
+#credentials = service_account.Credentials.from_service_account_info(
+#    st.secrets["gcp_service_account"],
+#    scopes=scope)
+#client = Client(scope=scope,creds=credentials)
 
-spreadsheetname = "Dataset_Intro_List"
-spread = Spread(spreadsheetname,client = client)
-sh = client.open(spreadsheetname)
-dataset = load_the_spreadsheet('8.Text Mining')
+#spreadsheetname = "Dataset_Intro_List"
+#spread = Spread(spreadsheetname,client = client)
+#sh = client.open(spreadsheetname)
+#dataset = load_the_spreadsheet('8.Text Mining')
+conn = st.connection("gsheets", type=GSheetsConnection)
+dataset = conn.read(worksheet="6.Building Information",usecols=[0,1])
+dataset = dataset.dropna(thresh=2)
 #%%
 dataset= dataset.reset_index(drop=True)
 
